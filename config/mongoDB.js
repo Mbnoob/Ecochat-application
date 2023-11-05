daafa8b7c46ae27da0fb7d056459bdda99867fb8
Mbnoob <maharajabain190@gmail.com>
2023-11-05 20:41:55 +0530

    updated

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('mongodb://localhost:27017/EcoChat-application').then((r)=>{
+const connection = mongoose.connect(process.env.MONGODB_URI).then((r)=>{
     console.log('Database is Connected Successfully');
 }).catch((e)=>{
     console.log(e)
